62cb81a90743571b54ea49f1a0d7162197f4a2bb
Muhammad Irfan <Muhammad Irfan>
2016-04-27 14:56:39 +0700

    added mongodb

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ var app = express();
 var sass = require("node-sass");
 var request = require("request");
 var bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
+var mongodb = require("mongodb").MongoClient;
+var db_url = "mongodb://"+ process.env.MONGODB_USER +":"+ process.env.MONGODB_PASSWORD +"@ds013951.mlab.com:13951/fcc-challenge";
+
 app.use('/dist', express.static(__dirname + '/dist'));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cookieParser());
 
 //define view engine
 app.set("views", __dirname + "/views");
@@ -34,23 +39,69 @@ app.get("/auth/github_callback", function(req,res){
 		function (err, response, body) {
 			if (err) res.end(err);
 			//put the token in cookie
-			res.cookie("token", req.query.code, {maxAge : 3600000});
+			res.cookie("token", req.query.code, {maxAge : 360000000});
 			res.redirect("/");
 		}
 	)
+});
+app.get("/findAllPoll", function(req,res){
+	mongodb.connect(db_url, function(err,db){
+		if (err) throw err;
+		
+		var collection = db.collection("voting_app");
+		collection.find(
+			{}
+		).toArray(function(err, docs){
+			if (err) throw err;
+			res.end(JSON.stringify(docs));
+			db.close();
+		});	
+	});
+	
+	
 });
 app.get("/signout", function(req,res){
 	res.clearCookie("token");
 	res.redirect("/");
 });
 app.post("/vote", function(req,res) {
-	console.log(req.body);
 	res.end(JSON.stringify({message : "success"}));
 });
 
 //todo : implement authorized only
-app.get("/createpoll", function(req,res){
-	res.end(JSON.stringify({message : "success"}));
+app.post("/createpoll", function(req,res){
+	if (!req.cookies.token) {
+		res.end(403, "not authorized");
+	}
+	var optArr = req.body.poll_options.split(",");
+	var result = {};
+	for (var i = 0; i < optArr.length; i++) {
+		result[optArr[i]] = 0;
+	}
+	//forming entry
+	var entry = {
+		author : req.cookies.token,
+		status : "active",
+		title : req.body.title,
+		description : req.body.description,
+		result : result,
+		ip_voters : [],
+		username_voters : []
+	};
+
+	mongodb.connect(db_url, function(err,db){
+		if (err) throw err;
+		
+		var collection = db.collection("voting_app");
+		collection.insert(
+			entry,
+			function(err, docs) {
+				if (err) throw err;
+				db.close();
+				res.end(JSON.stringify({message: "success"}));
+			}
+		);
+	});
 });
 app.get("*", function(req,res){
 	res.end("404!");
